b181d0db84d3fb9b2e75b535b0b7d61b1574ad6c
Reboot297 <reboot297@gmail.com>
2024-03-19 16:08:45 +0200

    added warnings for denied permissions

--- app/src/main/java/com/reboot297/pemissions/samples/PermissionsObserver.kt
+++ app/src/main/java/com/reboot297/pemissions/samples/PermissionsObserver.kt
@@ -1,7 +1,9 @@
 package com.reboot297.pemissions.samples
 
-import android.R
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
@@ -9,11 +11,12 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.snackbar.Snackbar
 
+private const val TAG = "PermissionsObserver"
 
 class PermissionsObserver(
     private val activity: ComponentActivity,
-    // TODO add map of rationale Messages, if empty = use default message
     private var permissions: Array<String> = arrayOf()
 ) : DefaultLifecycleObserver {
 
@@ -21,25 +24,26 @@ class PermissionsObserver(
         activity.registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { permissions: Map<String, Boolean> ->
-            permissions.forEach {
-                // TODO(Check all permissions)
-                if (it.value) {
-                    // Permission is granted. Continue the action or workflow in your
-                    // app.
-                } else {
-                    // Explain to the user that the feature is unavailable because the
-                    // feature requires a permission that the user has denied. At the
-                    // same time, respect the user's decision. Don't link to system
-                    // settings in an effort to convince the user to change their
-                    // decision.
-                }
+            val hasDenied = permissions.any { !it.value }
+            if (hasDenied) {
+                showSnackBar()
+            } else {
+                // all permissions granted
             }
-
         }
 
 
     override fun onCreate(owner: LifecycleOwner) {
         super.onCreate(owner)
+        initPermissions()
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        checkPermissions()
+    }
+
+    private fun initPermissions() {
         try {
             permissions = activity.packageManager.getPackageInfo(
                 activity.packageName, PackageManager.GET_PERMISSIONS
@@ -49,51 +53,63 @@ class PermissionsObserver(
         }
     }
 
-    override fun onResume(owner: LifecycleOwner) {
-        super.onResume(owner)
-        checkManifestPermissions()
-    }
+    private fun checkPermissions(): Boolean {
+        Log.d(TAG, "checkManifestPermissions: " + permissions.joinToString(","))
 
-    fun checkManifestPermissions(): Boolean {
-        permissions.forEach {
-            Log.d("TAG", "checkManifestPermissions: " + it)
-        }
-
-        val notGrantedPermissions: MutableList<String> = ArrayList()
-        var needsMessage = false
+        val requiredPermissions = mutableListOf<String>()
+        val warningPermissions = mutableListOf<String>()
         for (permission in permissions) {
             if (ActivityCompat.checkSelfPermission(
                     activity,
                     permission
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                notGrantedPermissions.add(permission)
-                needsMessage = (needsMessage || ActivityCompat.shouldShowRequestPermissionRationale(
-                    activity,
-                    permission
-                ))
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                    warningPermissions.add(permission)
+                } else {
+                    requiredPermissions.add(permission)
+                }
             }
         }
 
-        if (notGrantedPermissions.size > 0) {
-            requestPermissions(notGrantedPermissions.toTypedArray(), needsMessage)
+        if (requiredPermissions.isNotEmpty()) {
+            requestPermissions(requiredPermissions.toTypedArray())
         }
 
-        return notGrantedPermissions.size == 0
+        if (warningPermissions.isNotEmpty()) {
+            showRequestPermissionRationale(warningPermissions.toTypedArray())
+        }
+
+        return requiredPermissions.isNotEmpty()
     }
 
-    fun requestPermissions(permissions: Array<String>, needsMessage: Boolean) {
-        if (needsMessage) {
-            val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
-            //builder.setTitle(R.string.alert_permissions_title)
-            //builder.setMessage(R.string.alert_permissions_message)
-            builder.setCancelable(false)
-            builder.setPositiveButton(R.string.ok) { _, _ ->
-                requestPermissionLauncher.launch(permissions)
+    private fun showRequestPermissionRationale(permissions: Array<String>) {
+        AlertDialog.Builder(activity)
+            .setTitle(R.string.alert_permissions_title)
+            .setMessage(R.string.alert_permissions_message)
+            .setCancelable(false)
+            .setPositiveButton(R.string.alert_permissions_ok) { _, _ ->
+                requestPermissions(permissions)
             }
-            builder.show()
-        } else {
-            requestPermissionLauncher.launch(permissions)
-        }
+            .setNegativeButton(R.string.alert_permissions_cancel, null)
+            .show()
+    }
+
+    private fun requestPermissions(permissions: Array<String>) {
+        requestPermissionLauncher.launch(permissions)
+    }
+
+    private fun showSnackBar() {
+        Snackbar.make(
+            activity.window.decorView.findViewById(android.R.id.content),
+            R.string.permissions_denied_message,
+            Snackbar.LENGTH_LONG
+        ).setAction(R.string.action_settings) {
+
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", activity.packageName, null)
+            intent.setData(uri)
+            activity.startActivity(intent)
+        }.show()
     }
 }
